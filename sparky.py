05948e7645cac0ad83aa64906c2591fd6266db84
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-07-11 10:23:22 -0500

    eval usage tracking

--- sparky.py
+++ sparky.py
@@ -53,18 +53,16 @@ class Decoder(nn.Module):
         return embed
 
 class AutoEncoder(nn.Module):
-    def __init__(self, embed_size, num_features, topk, dead_cutoff=100_000, dead_topk=512, pin_bias=True):
+    def __init__(self, embed_size, num_features, topk, pin_bias=True):
         super().__init__()
         self.num_features = num_features
         self.topk = topk
-
-        self.dead_cutoff = dead_cutoff
-        self.dead_topk = dead_topk
 
         self.encoder = Encoder(embed_size, num_features)
         self.decoder = Decoder(embed_size, num_features)
 
         self.register_buffer('last_usage', torch.zeros(num_features, dtype=torch.int64))
+        self.register_buffer('eval_usage', torch.zeros(num_features, dtype=torch.int64))
 
         self.init_weights()
         if pin_bias:
@@ -77,23 +75,24 @@ class AutoEncoder(nn.Module):
         self.decoder.lookup.weight.data.normal_(0, 0.1)
 
     def reset_usage(self):
-        self.last_usage.fill_(0)
+        self.eval_usage.fill_(0)
 
     def features(self, embed):
         project = self.encoder(embed)
-        weights, feats = project.topk(self.topk, dim=-1)
+        weights, feats = project.topk(self.topk, dim=-1, sorted=False)
         return feats
 
     # embed: [batch_size, embed_size]
-    def forward(self, embed, undead=False):
+    def forward(self, embed, dead_cutoff=None, dead_topk=512):
         # sparsifry and index
         project = self.encoder(embed)
-        weights, feats = project.topk(self.topk, dim=-1)
+        weights, feats = project.topk(self.topk, dim=-1, sorted=False)
 
         # accumulate usage stats
         with torch.no_grad():
             batch_size, _ = embed.shape
             total = bincount(feats, self.num_features)
+            self.eval_usage += total
             self.last_usage += batch_size # increment all
             self.last_usage *= 1 - total.clamp(max=1) # zero out used
 
@@ -102,25 +101,24 @@ class AutoEncoder(nn.Module):
         embed1 = F.normalize(recon)
 
         # add in resurrected feats
-        if undead:
+        if dead_cutoff is not None:
             # find dead_topk least used features
-            dead_mask = self.last_usage > self.dead_cutoff
+            dead_mask = self.last_usage > dead_cutoff
             dead_total = dead_mask.sum().item()
             dead_project = project[:, dead_mask]
 
             # reconstruct undead embeddings
-            undead_topk = np.minimum(self.dead_topk, dead_total)
-            undead_weights, undead_feats = dead_project.topk(undead_topk, dim=-1)
+            undead_topk = np.minimum(dead_topk, dead_total)
+            undead_weights, undead_feats = dead_project.topk(undead_topk, dim=-1, sorted=False)
             undead_recon = self.decoder(undead_feats, undead_weights, bias=False)
             return embed1, undead_recon
         else:
-            return embed1
+            return embed1, None
 
 class Trainer:
-    def __init__(self, embed, model, undead_weight=0.03):
+    def __init__(self, embed, model):
         self.embed = embed
         self.model = model
-        self.undead_weight = undead_weight
         self.loss_fn = torch.nn.MSELoss()
 
     def features(self, text):
@@ -128,28 +126,25 @@ class Trainer:
             vecs = self.embed(text)
         return self.model.features(vecs)
 
-    def loss(self, text):
+    def loss(self, text, dead_cutoff=None, dead_topk=512, dead_weight=0.03):
+        # get embeddings and run model
         with torch.no_grad():
             vecs = self.embed(text)
-        bs, es = vecs.shape
-        vecs1, uvecs = self.model(vecs, undead=True)
+        vecs1, uvecs = self.model(vecs, dead_cutoff=dead_cutoff, dead_topk=dead_topk)
+
+        # compute loss
+        scale = sqrt(vecs.numel())
         loss0 = self.loss_fn(vecs, vecs1)
-        loss1 = self.loss_fn(vecs1 - vecs, uvecs)
-        return sqrt(bs * es) * (loss0 + self.undead_weight*loss1)
+        loss1 = self.loss_fn(vecs1 - vecs, uvecs) if uvecs is not None else 0.0
+        return scale * (loss0 + dead_weight*loss1)
 
     def train(
-        self, data, epochs=10, max_steps=None, learning_rate=1e-3, epsilon=1e-8, grad_clip=1.0
+        self, data, epochs=10, max_steps=None, learning_rate=1e-3, epsilon=1e-8, grad_clip=1.0,
+        eval_steps=8, dead_cutoff=10_000, dead_topk=512, dead_weight=0.03
     ):
         # set up optimizer
         params = self.model.parameters()
         optim = torch.optim.Adam(params, lr=learning_rate, eps=epsilon)
-
-        # evaluate some batches
-        def run_eval(ne):
-            with torch.no_grad():
-                return sum([
-                    self.loss(batch).item() for batch in islice(data, ne)
-                ]) / ne
 
         # run some data epochs
         for e in range(epochs):
@@ -157,7 +152,9 @@ class Trainer:
             for step, batch in enumerate(tqdm(data, desc=f'EPOCH {e}')):
                 # accumulate gradients
                 optim.zero_grad()
-                loss = self.loss(batch)
+                loss = self.loss(
+                    batch, dead_cutoff=dead_cutoff, dead_topk=dead_topk, dead_weight=dead_weight
+                )
                 loss.backward()
 
                 # process and apply grads
@@ -169,5 +166,15 @@ class Trainer:
                     break
 
             # print out loss
-            eval_loss = run_eval(8)
-            print(f'LOSS: {eval_loss}')
+            with torch.no_grad():
+                self.model.reset_usage()
+                eval_loss = sum([
+                    self.loss(b).item() for b in islice(data, eval_steps)
+                ]) / eval_steps
+                dead_frac = (self.model.eval_usage == 0).float().mean().item()
+            print(f'LOSS: {eval_loss}, DEAD: {dead_frac}')
+
+            # print out dead feature stats
+            usage_mean = self.model.last_usage.float().mean().item()
+            dead_feats = (self.model.last_usage > dead_cutoff).sum().item()
+            print(f'last_usage = {usage_mean}, dead_feats = {dead_feats}')
